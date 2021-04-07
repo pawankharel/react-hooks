f1a3c021e3070520201f7b6f6a856a8df140dcf3
Pawan Kharel <kharelpawan73@gmail.com>
2021-04-07 17:06:53 +0545

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Increment from './Increment';
+import Datafetch from './Datafetch';
+
+function App() {
+  return (
+    <div className="App">
+      <Increment/>
+      <Datafetch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Increment.js
@@ -0,0 +1,23 @@
+import React,{useState} from 'react';
+
+const Increment=()=> {
+    const [count, setCount]=useState(0)
+    // const increase=()=>{
+    //     setCount(count+1)
+    // }
+
+
+    return (
+        <>
+
+            <h1>{count}</h1>
+            {count<50 &&<button onClick={
+                ()=>{
+                    setCount(count+1)}}>Click to increase value</button>}
+
+                    {count>0 &&<button onClick={()=>count>0 && setCount(count-1)}>Click to decrease value</button>}
+        </>
+    )
+                }
+
+export default Increment
